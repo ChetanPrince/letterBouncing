fd62b945458ac855fc5c12cdbdd7cda357e68359
ChetanPrince <saini.rajvir2@gmail.com>
2024-08-21 23:52:02 +0200

    3

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-const keyword = document.querySelector(".container h1").value;
-const letters = keyword.split("").join(",");
-console.log(letters);
+const keywordElement = document.querySelector(".container h1");
+const keyword = keywordElement.textContent;
+
+// Wrap each letter in a span
+keywordElement.innerHTML = keyword.split("").map(letter => `<span>${letter}</span>`).join("");
+
+// Select all the span elements
+const letterSpans = keywordElement.querySelectorAll("span");
+
+// Add event listeners to each span
+letterSpans.forEach((span) => {
+    span.addEventListener("mouseover", () => {
+        span.classList.add("change");
+    });
+});
