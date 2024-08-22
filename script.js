4c02bad86a30332f632f06f71c707efe8b5d13f8
ChetanPrince <saini.rajvir2@gmail.com>
2024-08-22 07:23:14 +0200

    bounce effect on hover is completed through javascript

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 const keywordElement = document.querySelector(".container h1");
 const keyword = keywordElement.textContent;
 
-// Wrap each letter in a span
 keywordElement.innerHTML = keyword.split("").map(letter => `<span>${letter}</span>`).join("");
 
-// Select all the span elements
 const letterSpans = keywordElement.querySelectorAll("span");
 
-// Add event listeners to each span
 letterSpans.forEach((span) => {
     span.addEventListener("mouseover", () => {
         span.classList.add("change");
+        setTimeout(()=>{
+            span.classList.remove("change");
+
+        }, 1000)
     });
 });
+
+
+// alternate option of not having setTimeout() method on elements
+// letterSpans.forEach((span) => {
+//     span.addEventListener("mouseout", () => {
+//         span.classList.remove("change");
+//     });
+// });
